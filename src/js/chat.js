98b472c9c8007035dffeba94cb12a5953970fe3f
Code & Copy - Zander <zander@codeandcopy.co.za>
2024-05-16 18:42:02 +0200

    Refactor error handling and toast messages in footer.php

--- src/js/chat.js
+++ src/js/chat.js
@@ -1,24 +1,26 @@
-
 $(document).ready(function() {
-   try{
-    streams.forEach(function(stream, index) {
-        var videoId = 'video-' + index;
-        var videoElement = document.getElementById(videoId);
-        if (Hls.isSupported()) {
-            initializeHls(stream.url, videoElement, index);
-        }
-    });
-   }catch(e){
-    throw new Error(e);
-   }
+    try {
+        // Initialize HLS for each stream
+        streams.forEach(function(stream, index) {
+            var videoId = 'video-' + index;
+            var videoElement = document.getElementById(videoId);
+            if (Hls.isSupported()) {
+                initializeHls(stream.url, videoElement, index);
+            }
+        });
+    } catch (e) {
+        throw new Error(e);
+    }
 
-    
+    /**
+     * Initialize HLS for a given video element and stream URL.
+     */
     function initializeHls(url, videoElement, index) {
-
         var hls = new Hls({ maxBufferLength: 5 });
-        window[`hlsInstance${index}`] = hls; // Storing HLS instance to window for potential cleanup
+        window[`hlsInstance${index}`] = hls; // Store HLS instance for potential cleanup
         hls.loadSource(url);
         hls.attachMedia(videoElement);
+
         hls.on(Hls.Events.MANIFEST_PARSED, function() {
             if (videoElement.readyState > 2) { // 2 = HAVE_CURRENT_DATA
                 videoElement.play();
@@ -28,221 +30,253 @@ $(document).ready(function() {
                 };
             }
         });
+
         hls.on(Hls.Events.ERROR, function(event, data) {
             handleHlsErrors(data, hls, stream, index);
-          
         });
-
-       
     }
 
-
-
-    
-
-$('#messageBox').on('keypress', function(event) { 
-if (event.which === 13) { // 13 is the Enter key
-    event.preventDefault(); // Prevent default submit
-    // Optional: Add any checks or processing before the form submission
-    $(this).submit(); // Submit the form
-}
-});
-
-
-
-function moveChat() {
-    var $chatContainer = $('#messageDiv');
-    if ($(window).width() >= 768) { // Bootstrap's MD breakpoint
-        $('#desktop-chat-holder').append($chatContainer);
-        $('#mobileChatButton').addClass('d-none');
-        $('#chatModal').modal('hide');
-        $('#desktopChat').removeClass('d-none');
-    } else {
-        $('#mobile-chat-holder').append($chatContainer);
-        $('#mobileChatButton').removeClass('d-none');
-        $('#desktopChat').addClass('d-none');
+    /**
+     * Handle HLS errors and try to recover stream.
+     */
+    function handleHlsErrors(data, hls, stream, index) {
+        console.error('HLS error:', data);
+        if (data.fatal) {
+            switch (data.type) {
+                case Hls.ErrorTypes.NETWORK_ERROR:
+                    console.error('Network error, trying to recover...');
+                    hls.startLoad();
+                    break;
+                case Hls.ErrorTypes.MEDIA_ERROR:
+                    console.error('Media error, trying to recover...');
+                    hls.recoverMediaError();
+                    break;
+                default:
+                    console.error('Unrecoverable error, destroying HLS instance...');
+                    hls.destroy();
+                    initializeHls(stream.url, document.getElementById('video-' + index), index);
+                    break;
+            }
+        }
     }
-}
 
-// Initial move
-moveChat();
-
-// Re-apply on window resize
-$(window).on('resize', moveChat);
-
-// clear count and set message pill to d-none and text html to empty
-$('#mobileChatButton').on('click', function() {
-    $('#messagePill').addClass('d-none');
-    $('#messageCount').html(0);
-    scrollDown();
-});
-
- // Register socket event handlers
- socket.on('connect', function() {
-    socket.emit('joinProjectRoom', { 'sessionId': sessionId });
-    fetchAllMessages(sessionId);
-
-});
-
-socket.on('newMessage', function(data) {
-    fetchLatestMessage(sessionId);
-});
-
-$('#messageBox').on('submit', function(e) {
-    e.preventDefault();
-    sendMessage(sessionId);
-    $('#messageCount').text(-1);
-});
-
-function sendMessage(sessionId) {
-    const text = $('#messageInput').val();
-    if (text) {
-        socket.emit('sendMessage', { text, session: sessionId });
-        $('#messageInput').val("");
+    /**
+     * Move chat container based on window width.
+     */
+    function moveChat() {
+        var $chatContainer = $('#messageDiv');
+        if ($(window).width() >= 768) { // Bootstrap's MD breakpoint
+            $('#desktop-chat-holder').append($chatContainer);
+            $('#mobileChatButton').addClass('d-none');
+            $('#chatModal').modal('hide');
+            $('#desktopChat').removeClass('d-none');
+        } else {
+            $('#mobile-chat-holder').append($chatContainer);
+            $('#mobileChatButton').removeClass('d-none');
+            $('#desktopChat').addClass('d-none');
+        }
     }
-}
 
-function setMessageCount(newValue,oldValue){
-  
-    let val = parseInt(newValue)+parseInt(oldValue);
-    if(val>0){
-    $('#messagePill').removeClass('d-none');
-    $('#messageCount').text(val);
-    }else{
+    // Initial move
+    moveChat();
+
+    // Re-apply on window resize
+    $(window).on('resize', moveChat);
+
+    // Clear count and set message pill to d-none and text HTML to empty
+    $('#mobileChatButton').on('click', function() {
         $('#messagePill').addClass('d-none');
+        $('#messageCount').html(0);
+        scrollDown();
+    });
+
+    // Register socket event handlers
+    socket.on('connect', function() {
+        socket.emit('joinProjectRoom', { 'sessionId': sessionId });
+        fetchAllMessages(sessionId);
+    });
+
+    socket.on('newMessage', function(data) {
+        fetchLatestMessage(sessionId);
+    });
+
+    // Send message on form submit
+    $('#messageBox').on('submit', function(e) {
+        e.preventDefault();
+        sendMessage(sessionId);
+        $('#messageCount').text(-1);
+    });
+
+    // Handle Enter key press in message box
+    $('#messageBox').on('keypress', function(event) {
+        if (event.which === 13) { // 13 is the Enter key
+            event.preventDefault(); // Prevent default submit
+            $(this).submit(); // Submit the form
+        }
+    });
+
+    /**
+     * Send a message to the server.
+     */
+    function sendMessage(sessionId) {
+        const text = $('#messageInput').val();
+        if (text) {
+            socket.emit('sendMessage', { text, session: sessionId });
+            $('#messageInput').val("");
+        }
     }
-}
 
-function fetchAllMessages(sessionId) {
-    $.ajax({
-        url: 'src/php/chat.php?get=all-messages',
-        method: 'POST',
-        data: { session: sessionId },
-        success: function(response) {
-            response = JSON.parse(response);
-         
-            displayMessages(response.body.messages.slice(0, -1));
-            let newValue = response.body.messages.length;
-            setMessageCount(newValue,"0");
-        },
-        error: function(error) {
-            console.error('Error fetching all messages:', error);
+    /**
+     * Set the message count and update the UI.
+     */
+    function setMessageCount(newValue, oldValue) {
+        let val = parseInt(newValue) + parseInt(oldValue);
+        if (val > 0) {
+            $('#messagePill').removeClass('d-none');
+            $('#messageCount').text(val);
+        } else {
+            $('#messagePill').addClass('d-none');
         }
-    });
-}
+    }
 
-function fetchLatestMessage(sessionId) {
-    $.ajax({
-        url: 'src/php/chat.php?get=latest-message',
-        method: 'POST',
-        data: { session: sessionId },
-        success: function(response) {
-            response = JSON.parse(response);
-           
-            displayMessage(response.body.messages);
-            let newValue = 1;
-            let oldvalue = parseInt($('#messageCount').text());
-            setMessageCount(newValue,oldvalue);
+    /**
+     * Fetch all messages for a session.
+     */
+    function fetchAllMessages(sessionId) {
+        $.ajax({
+            url: 'src/php/chat.php?get=all-messages',
+            method: 'POST',
+            data: { session: sessionId },
+            success: function(response) {
+                response = JSON.parse(response);
+                displayMessages(response.body.messages.slice(0, -1));
+                let newValue = response.body.messages.length;
+                setMessageCount(newValue, "0");
+            },
+            error: function(error) {
+                console.error('Error fetching all messages:', error);
+            }
+        });
+    }
 
-        },
-        error: function(error) {
-            console.error('Error fetching latest message:', error);
-        }
-    });
-}
-const msgBox = (msg) => {
-  return `<div class="w-100 rounded-3 bg-white shadow-sm p-2 px-3 mb-2">
+    /**
+     * Fetch the latest message for a session.
+     */
+    function fetchLatestMessage(sessionId) {
+        $.ajax({
+            url: 'src/php/chat.php?get=latest-message',
+            method: 'POST',
+            data: { session: sessionId },
+            success: function(response) {
+                response = JSON.parse(response);
+                displayMessage(response.body.messages);
+                let newValue = 1;
+                let oldValue = parseInt($('#messageCount').text());
+                setMessageCount(newValue, oldValue);
+            },
+            error: function(error) {
+                console.error('Error fetching latest message:', error);
+            }
+        });
+    }
+
+    /**
+     * Create a message box HTML string.
+     */
+    const msgBox = (msg) => {
+        return `
+            <div class="w-100 rounded-3 bg-white shadow-sm p-2 px-3 mb-2">
                 <div class="messageContent">${msg.message}</div>
-              <div class="messageMeta d-flex justify-content-end border-top">
-              <span class="messageUser small">${msg.viewerName}</span>&nbsp;
-              <span class="small">@</span>&nbsp;
-                <span class="messageTime small">${new Date(msg.sent).toLocaleString('en-ZA')}</span>
-               
-              </div>
+                <div class="messageMeta d-flex justify-content-end border-top">
+                    <span class="messageUser small">${msg.viewerName}</span>&nbsp;
+                    <span class="small">@</span>&nbsp;
+                    <span class="messageTime small">${new Date(msg.sent).toLocaleString('en-ZA')}</span>
+                </div>
             </div>
-        `
-}
-
-function displayMessages(messages) {
-    messages.forEach(msg => {
-        $('#messages').append(msgBox(msg));
-    });
-}
-
-function displayMessage(message) {
-    $('#messages').append(msgBox(message));
-}
-
-// Setup auto-scroll
-const observer = new MutationObserver(function() {
-    const messagesDiv = document.getElementById('messages');
-    messagesDiv.scrollTop = messagesDiv.scrollHeight;
-});
-observer.observe(document.getElementById('messages'), { childList: true });
-
-
-function scrollDown(){
-var $messages = $('#messages');
-    $messages.scrollTop($messages.prop("scrollHeight"));
-}
-
-
-
-$(".spinner").addClass("d-none");
-
-
-
-});
-errorFlag = false;
-
-
-window.addEventListener('error', function(e) {
-console.log(e.filename)
-});
-
-
-window.onerror = function (message, source, lineno, colno, error) {
-console.log(message)
-   if(source.includes("video.min.js") || source.includes("hls.min.js")){
-    if(!errorFlag){
-        errorFlag = true;
-        showToast("bg-danger", "Stream terminated trying to recover...")
-        showToast("bg-success", "Refreshing...")
-        setTimeout(function(){
-            window.location.reload();
-        }, 5000);
-    }
-    streams.forEach(function(stream, index) {
-        var videoId = 'video-' + index;
-        var videoElement = document.getElementById(videoId);
-        if (Hls.isSupported()) {
-            initializeHls(stream.url, videoElement, index);
-            
-        }
-    });
-
-    checkAllVideoPlayers();
-}
-   console.clear();
-    // Return `true` to prevent the default browser error message from being displayed
-    return true;
-};
-
-// Function to check all Video.js players on the page
-function checkAllVideoPlayers() {
-    const allPlayers = videojs.players; // Get all players
-    const playerStates = [];
-
-    // Iterate through each player
-    for (let playerId in allPlayers) {
-        if (allPlayers.hasOwnProperty(playerId)) {
-            const player = allPlayers[playerId];
-            // Check if the player is playing (not paused)
-            const isPlaying = !player.paused();
-            playerStates.push({ playerId, isPlaying });
-        }
+        `;
     }
 
-    return playerStates; // Optional: return the states if needed elsewhere
-}
+    /**
+     * Display multiple messages.
+     */
+    function displayMessages(messages) {
+        messages.forEach(msg => {
+            $('#messages').append(msgBox(msg));
+        });
+    }
 
+    /**
+     * Display a single message.
+     */
+    function displayMessage(message) {
+        $('#messages').append(msgBox(message));
+    }
+
+    // Setup auto-scroll for messages div
+    const observer = new MutationObserver(function() {
+        const messagesDiv = document.getElementById('messages');
+        messagesDiv.scrollTop = messagesDiv.scrollHeight;
+    });
+    observer.observe(document.getElementById('messages'), { childList: true });
+
+    /**
+     * Scroll down to the bottom of the messages div.
+     */
+    function scrollDown() {
+        var $messages = $('#messages');
+        $messages.scrollTop($messages.prop("scrollHeight"));
+    }
+
+    // Hide spinner after initialization
+    $(".spinner").addClass("d-none");
+
+    // Global error flag
+    let errorFlag = false;
+
+    // Global error handler
+    window.addEventListener('error', function(e) {
+        console.log(e.filename);
+    });
+
+    window.onerror = function(message, source, lineno, colno, error) {
+        console.log(message);
+        if (source.includes("video.min.js") || source.includes("hls.min.js")) {
+            if (!errorFlag) {
+                errorFlag = true;
+                showToast("bg-danger", "Stream terminated, trying to recover...");
+                showToast("bg-success", "Refreshing...");
+                setTimeout(function() {
+                    window.location.reload();
+                }, 5000);
+            }
+            streams.forEach(function(stream, index) {
+                var videoId = 'video-' + index;
+                var videoElement = document.getElementById(videoId);
+                if (Hls.isSupported()) {
+                    initializeHls(stream.url, videoElement, index);
+                }
+            });
+            checkAllVideoPlayers();
+        }
+        console.clear();
+        return true; // Prevent the default browser error message
+    };
+
+    /**
+     * Check all Video.js players on the page.
+     */
+    function checkAllVideoPlayers() {
+        const allPlayers = videojs.players; // Get all players
+        const playerStates = [];
+
+        for (let playerId in allPlayers) {
+            if (allPlayers.hasOwnProperty(playerId)) {
+                const player = allPlayers[playerId];
+                const isPlaying = !player.paused();
+                playerStates.push({ playerId, isPlaying });
+            }
+        }
+
+        return playerStates; // Optional: return the states if needed elsewhere
+    }
+});
